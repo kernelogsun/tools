b49201d6345b9c3c5783f0e692bc2e8bd57ef315
Ryohei Suzuki <RS.A.Management.256@gmail.com>
2018-07-21 17:11:35 +0900

    Added note of debugging method

--- debugging/start_gdbserver.sh
+++ debugging/start_gdbserver.sh
@@ -26,4 +26,26 @@ if [ $? = 1 ]; then
 fi
 
 
+cat << EOS
+--- Note ---
+
+e.g.
+1. gdb
+2. (gdb) file /usr/lib/debug/lib/modules/<version>/vmlinux
+3. (gdb) add-symbole-file /usr/lib/debug/lib/modules/<version>/kernel/fs/xfs/xfs.ko.debug <module address of connection guest (*1)>
+4. (gdb) b xfs_sysctl_register
+5. (gdb) target remote :1234
+6. (gdb) c
+
+----
+
+(*1) How to check a module address
+
+     1. login guest os
+     2. modprobe xfs (if not load)
+     3. cat /sys/module/xfs/sections/.text
+        0xffffffffc0e7f000
+
+-----------
+EOS
 virsh qemu-monitor-command --hmp ${domain} gdbserver
